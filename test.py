9b006f61a191307863701cab8767b7e79b01fab4
Kevin TELLIER <kevincf.tellier@gmail.com>
2018-06-01 15:36:45 +0200

    Improved web interface

--- test.py
+++ test.py
@@ -15,15 +15,31 @@ def upload_file():
 		file = request.files['file']
 		new_x = request.form['x_red']
 		new_y = request.form['y_red']
-		print(file)
-		filename = secure_filename(file.filename)
-		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-		os.system("python script.py "+filename +" "+new_x +" "+ new_y)
-		filepath = (os.path.join('static/uploads', filename))
-		return render_template('upload_complete.html', file="/static/img/result.png", xred=new_x, yred =new_y)
+		option = request.form['operation']
+
+		if(option == 'mask'):
+			#Cas mask
+			option = "Masque"
+			filename = secure_filename(file.filename)
+			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+			filepath = (os.path.join('static/uploads', filename))
+			file2 = request.files['file2']
+			filename2 = secure_filename(file2.filename)
+			file2.save(os.path.join(app.config['UPLOAD_FOLDER'], filename2))
+			filepath2 = (os.path.join('static/uploads', filename2))
+			#os.system("python script.py "+filename +" "+new_x +" "+ new_y)
+			return render_template('upload_complete.html', file=filename, xred=new_x, yred =new_y, operation= option, file2=filename2)
+		else:
+			#Cas resize
+			option = "Resize"
+			filename = secure_filename(file.filename)
+			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+			filepath = (os.path.join('static/uploads', filename))
+			#os.system("python script.py "+filename +" "+new_x +" "+ new_y)
+			return render_template('upload_complete.html', file=filename, xred=new_x, yred =new_y, operation= option, file2="")
 	return render_template('error.html')
 
-@app.route("/")
+@app.route("/")	
 def index():
 	return render_template('index.html')
 
