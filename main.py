a5e69c7e94bf6a867bed855b22d11ffddde0cc86
kevin <kevincf.tellier@gmail.com>
2018-06-13 22:57:13 +0200

    Interface v2 mask upload not working

---
+++ main.py
@@ -0,0 +1,74 @@
+import os
+import sys
+from flask import Flask, render_template, request, redirect, url_for, flash
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+UPLOAD_FOLDER = os.path.join(APP_ROOT, 'static/uploads')
+
+filename = ""
+filename2 = ""
+new_x = 0
+new_y = 0
+option = ""
+
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route("/", methods=['POST'])
+def upload_file():
+	if request.method == 'POST':
+		option = request.form['operation']
+		print(option, file=sys.stderr)
+
+		if option == "mask":
+			#Mask case
+			option = "Masque"
+			file = request.files['file']
+			file2 = request.files['file2']
+
+			filename = secure_filename(file.filename)
+			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+			filepath = (os.path.join('static/uploads', filename))
+			filename2 = secure_filename(file2.filename)
+			file2.save(os.path.join(app.config['UPLOAD_FOLDER'], filename2))
+			filepath2 = (os.path.join('static/uploads', filename2))
+			reader = os.system("python script.py")
+			#print(reader, file=sys.stderr)
+			return render_template('upload_complete.html', file=filename, xred="", yred ="", operation= option, file2=filename2)
+
+		elif option == "resize":
+			#Resize case
+			option = "Resize"
+			file = request.files['file']
+			new_x = request.form['x_red']
+			new_y = request.form['y_red']
+
+			filename = secure_filename(file.filename)
+			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+			filepath = (os.path.join('static/uploads', filename))
+			reader = os.system("python script.py "+filename)
+			return render_template('upload_complete.html', file=filename, xred=new_x, yred =new_y, operation= option, file2="")
+
+		elif option == "accent":
+			option = "Accentuation"
+			file = request.files['file']
+			new_x = request.form['x_red']
+			new_y = request.form['y_red']
+
+			filename = secure_filename(file.filename)
+			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+			filepath = (os.path.join('static/uploads', filename))
+			reader = os.system("python script.py "+filename)
+			return render_template('upload_complete.html', file=filename, xred=new_x, yred =new_y, operation= option, file2="")
+	return render_template('error.html')
+
+
+@app.route("/")	
+def index():
+	return render_template('index.html')
+
+if __name__ == '__main__':
+	app.run(debug=True)
--- static/js/form.js
+++ static/js/form.js
@@ -1,20 +1,17 @@
-var inputs = document.querySelectorAll( '.inputfile' );
-Array.prototype.forEach.call( inputs, function( input )
-{
-	var label	 = input.nextElementSibling,
-		labelVal = label.innerHTML;
+var fileInput = document.querySelector('#file');
+    fileInput.addEventListener('change', function() {
 
-	input.addEventListener( 'change', function( e )
-	{
-		var fileName = '';
-		if( this.files && this.files.length > 1 )
-			fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
-		else
-			fileName = e.target.value.split( '\\' ).pop();
+        var reader = new FileReader();
 
-		if( fileName )
-			label.querySelector( 'span' ).innerHTML = fileName;
-		else
-			label.innerHTML = labelVal;
-	});
-});
+        if(reader.readyState == 2){
+            	alert('OK1')
+            }
+        reader.addEventListener('done', function() {
+            if(reader.readyState == 2){
+            	alert('OK')
+            }
+        });
+
+        reader.readAsText(fileInput.files[0]);
+
+ 	});
